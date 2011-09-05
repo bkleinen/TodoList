afc530e3521f18c5089592bac6c3884a34ab941b
Kerstin Puschke <devel@kpuschke.eu>
2011-09-05 12:25:28 +0200

    auto-generated rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoList::Application.config.secret_token = '944597585c22a86774e8e30563fedc3bdb8d2582a06ec794b2b376f6034c63bb4a4129938c3562df1ce05012eefe65c66d2b352eeb66ce747a264154bd5e2935'
