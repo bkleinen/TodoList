07e44b99c07163e79f8ec430149f5e53a396e975
Kerstin Puschke <devel@kpuschke.eu>
2011-09-06 14:24:30 +0200

    syntax gefixt application.rb

--- config/application.rb
+++ config/application.rb
@@ -44,9 +44,10 @@ module TodoList
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+    end
   end
 end
 
-config.generators do |g|
-  g.test_framework :rspec, :fixture => false
-end
