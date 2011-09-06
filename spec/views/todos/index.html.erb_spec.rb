ebe7cf7d51f4a6bd51ea60d269c82ef6005eab57
Kerstin Puschke <devel@kpuschke.eu>
2011-09-06 15:34:45 +0200

    unit test view todos index ohne status und description

--- spec/views/todos/index.html.erb_spec.rb
+++ spec/views/todos/index.html.erb_spec.rb
@@ -20,9 +20,5 @@ describe "todos/index.html.erb" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Title".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Description".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => false.to_s, :count => 2
   end
 end
