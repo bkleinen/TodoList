6972cbb6f574688e2b251de995a7fe13034cd171
Kerstin Puschke <devel@kpuschke.eu>
2011-09-08 12:31:03 +0200

    unit tests user views nachgebessert

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -14,11 +14,5 @@ describe "users/show.html.erb" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Login/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Persistence Token/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Crypted Password/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Password Salt/)
   end
 end
